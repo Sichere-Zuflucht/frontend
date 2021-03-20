1a6b90e1af6b4f2998e94b4ad1b2f1f1d198926e
Sebastian Fellner <fellnerseb@gmail.com>
2021-03-20 03:28:21 +0100

    firebase stuff works

---
+++ components/Signup.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-form v-model="valid" style="width: 100%">
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col>
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col>
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col><v-btn @click="signup"> Sign UP </v-btn> </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    firstname: '',
+    lastname: '',
+    nameRules: [
+      (v) => !!v || 'Name is required',
+      (v) => v.length <= 10 || 'Name must be less than 10 characters',
+    ],
+    email: '',
+    emailRules: [
+      (v) => !!v || 'E-mail is required',
+      (v) => /.+@.+/.test(v) || 'E-mail must be valid',
+    ],
+  }),
+  methods: {
+    signup() {
+      window.$nuxt.$fire.auth.createUserWithEmailAndPassword(
+        this.email,
+        this.lastname
+      )
+    },
+  },
+}
+</script>
