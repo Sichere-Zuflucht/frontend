c8db1e531a9009810fbbbde37c2c7e151e99b1f6
ulrichbenedikt <ulrichbenedikt@gmail.com>
2021-03-21 17:27:47 +0100

    startseite

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <v-row justify="center" align="center"> Hello </v-row>
+  <v-row justify="center" align="center">
+    <v-img
+      lazy-src="https://assets-global.website-files.com/5e95aa93bef5360e2788a86e/5e9e9727e2658e68125e8b48_header-berlin.jpg"
+      src="https://assets-global.website-files.com/5e95aa93bef5360e2788a86e/5e9e9727e2658e68125e8b48_header-berlin.jpg"
+    ></v-img>
+    <v-sheet
+      color="white"
+      class="ma-auto"
+      elevation="0"
+      height="100"
+      width="300"
+      ><div class="text-h3 ma-auto">
+        Willkommen bei Sichere Zuflucht
+      </div></v-sheet
+    >
+  </v-row>
 </template>
