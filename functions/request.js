c5ef23b126eff2f6a967e5c2c07cb95bcec94a9b
Sebastian Fellner <fellnerseb@gmail.com>
2021-04-06 17:22:24 +0200

    visualisation in coach works

--- functions/request.js
+++ functions/request.js
@@ -2,28 +2,41 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const { coachInitialMail } = require('./emailTemplates')
 
-exports.sendRequest = functions.https.onCall((data, context) => {
-  admin
+exports.sendRequest = functions.https.onCall(async (data, context) => {
+  const womanData = await admin
+    .firestore()
+    .collection('users')
+    .doc(context.auth.uid)
+    .get()
+    .then((doc) => doc.data())
+
+  await admin
     .firestore()
     .collection('requests')
     .add({
       eMails: [coachInitialMail(data.coachName, data.message, data.coachUID)],
       coachAnswered: false,
+      coachId: data.coachUID,
+      womanId: context.auth.uid,
+      ids: [data.coachUID, context.auth.uid],
+      message: data.message,
+      dates: [],
+      womanUserName: womanData.userName,
+      womanAvatar: womanData.avatar,
     })
-    .then(function (docRef) {
-      admin
-        .firestore()
-        .collection('users')
-        .doc(context.auth.uid)
-        .collection('requests_new')
-        .doc(docRef.id)
-        .set({ ref: docRef })
-      admin
-        .firestore()
-        .collection('users')
-        .doc(data.coachUID)
-        .collection('requests_new')
-        .doc(docRef.id)
-        .set({ ref: docRef })
+})
+
+exports.getRequests = functions.https.onCall((data, context) => {
+  return admin
+    .firestore()
+    .collection('requests')
+    .where('ids', 'array-contains', context.auth.uid)
+    .get()
+    .then((snap) => {
+      const data = []
+      snap.forEach((doc) => {
+        data.push(doc.data())
+      })
+      return data
     })
 })
