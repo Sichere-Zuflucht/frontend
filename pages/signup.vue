0e126026d129e916dfa99807aacebfd00364f9d0
Sebastian Fellner <fellnerseb@gmail.com>
2021-05-02 19:27:15 +0200

    fix infinite loop

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="d-flex flex-column">
     <h1 class="text-center text-h1 my-4 primary--text">Anmelden</h1>
-    <Signup></Signup>
+    <SignupComponent></SignupComponent>
     <v-btn text to="reset-password" color="grey">Passwort vergessen</v-btn>
   </v-container>
 </template>
