39f87fadf50d597ab2d7bc8948c278b980c93165
ulrichbenedikt <ulrichbenedikt@gmail.com>
2021-09-16 18:02:58 +0200

    creating beratung page

---
+++ pages/beratung.vue
@@ -0,0 +1,234 @@
+<template>
+  <div>
+    <v-container class="py-md-16">
+      <div class="text-center">
+        <img />
+        <h1 class="text-h1 primary--text">
+          Werden Sie Teil von Sichere Zuflucht!
+        </h1>
+        <p class="caption">
+          Unsere Plattform ermöglicht Frauen in Gewaltbeziehungen sich anonym
+          Beratung zu holen – sicher, schnell, anonym und deutschlandweit.
+        </p>
+        <div class="d-flex align-center justify-center">
+          <v-btn color="secondary">Ja, ich möchte beraten</v-btn>
+        </div>
+        <h2 class="text-h2 secondary--text">
+          Warum brauchen gewaltbetroffene Frauen Ihre Unterstützung?
+        </h2>
+        <h3 class="text-h3 primary--text">Die Fakten</h3>
+        <img />
+        <p class="grey--text">
+          Jede 3. Frau ist betroffen! D.h. jeder von uns kennt mindestens EINE.
+        </p>
+        <img />
+        <p class="grey--text">
+          Frauenhäuser und Beratungsangebote melden seit Jahren, dass sie der
+          Nachfrage nicht gerecht werden können.
+        </p>
+      </div>
+      <ul>
+        <li>
+          Häusliche Gewalt ist immer noch ein weit verbreitetes
+          gesellschaftliches Problem mit
+          <b>großem Stigma und hoher Dunkelziffer.</b>
+        </li>
+        <li>
+          Eine EU-weite Dunkelfeld-Erhebung hat ergeben, dass
+          <b>22% der Frauen</b>, die in einer Beziehung mit einem Mann sind oder
+          waren, <b>körperliche Gewalt</b> erfahren haben und das dass zwei von
+          fünf Frauen <b>(43 %)</b> einer Form
+          <b>psychischer Gewalt</b> entweder durch den derzeitigen oder einen
+          früheren Partner ausgesetzt waren. Die Studie “Gewalt gegen Frauen in
+          Paarbeziehungen” vom BMFSFJ zeigt identische Zahlen und Fakten auf.
+          Frauenhäuser und fachspezifische Beratung sind ein essenzieller Teil
+          der Lösung, um diesem Missstand entgegen zu wirken. Aber sie reichen
+          nicht aus. Die Angebote sind regional sehr unterschiedlich.
+          Frauenhäuser und Beratungsstellen bieten ausschließlich lokale
+          Lösungen an – aber das Thema Gewalt bedarf mitunter der Anonymität,
+          die im lokalen Umfeld nicht zuverlässig gegeben ist. Die vorhandenen
+          Online-Angebote zu diesem Thema zielen meist auf Bekanntmachung
+          bestehender Beratungs-Angebote wie z.B. Hotlines ab. Sie bieten nur
+          selten direkt Unterstützung in digitaler Form an.
+        </li>
+      </ul>
+      <v-btn plain color="primary">weiterlesen</v-btn>
+    </v-container>
+    <v-divider />
+    <v-container>
+      <h2 class="text-h2 secondary--text text-center">
+        An wen richtet sich Sichere Zuflucht?
+      </h2>
+      <p class="text-center">
+        Jede von Gewalt betroffene Frau – <b>egal</b> in welchem <b>Alter</b>,
+        welcher <b>Herkunft</b> oder <b>Religion</b>, unabhängig von
+        <b>Bildungsstand</b> und <b>Einkommen</b> – verdient ein für sie leicht
+        erreichbares <b>Unterstützungsangebot.</b>
+      </p>
+      <v-row
+        ><v-col>
+          <v-card>
+            <img />
+            <v-card-text>
+              <p class="subtitle-2 primary--text">Katrin (30) + Leni (3)</p>
+              <p class="caption">Krankenschwester</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <img />
+            <v-card-text>
+              <p class="subtitle-2 primary--text">Heidi (73)</p>
+              <p class="caption">Rentnerin</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <img />
+            <v-card-text>
+              <p class="subtitle-2 primary--text">Angelika (43)</p>
+              <p class="caption">Anwältin</p>
+            </v-card-text>
+          </v-card>
+        </v-col></v-row
+      >
+      <v-row
+        ><v-col>
+          <v-card>
+            <img />
+            <v-card-text>
+              <p class="subtitle-2 primary--text">Nasrin (17)</p>
+              <p class="caption">Abiturientin</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <img />
+            <v-card-text>
+              <p class="subtitle-2 primary--text">Sanya (34) + Sam (8)</p>
+              <p class="caption">Köchin</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <img />
+            <v-card-text>
+              <p class="subtitle-2 primary--text">Llian (29) + Xi (4)</p>
+              <p class="caption">Sekretärin</p>
+            </v-card-text>
+          </v-card>
+        </v-col></v-row
+      >
+      <ul>
+        <li>
+          Frauen mit einem höheren Bildungshintergrund, Erfolg im Beruf und
+          eigenem Einkommen – die also
+          <b>fest im Leben stehen und sozial integriert</b> sind –
+          <b>schrecken häufig vor ihnen bekannten Hilfsangeboten zurück.</b>
+        </li>
+        <li>
+          Aufgrund ihres <b>Schamgefühls</b> fällt es ihnen besonders schwer
+          Unterstützung in Frauenhäusern zu suchen oder sich an lokale
+          Hilfsangebote zu wenden.
+        </li>
+        <li>
+          <b>Sichere Zuflucht</b> hat es sich zur Aufgabe gemacht diesen Frauen
+          eine für sie <b>passende Unterstützung</b> anzubieten.
+        </li>
+      </ul>
+    </v-container>
+    <v-divider />
+    <v-container>
+      <h2 class="text-h2 text-center secondary--text">Themenbereiche</h2>
+      <h3 class="text-h3 text-center primary--text">
+        wo können Sie mit Ihren Kompetenzen beraten?
+      </h3>
+      <p color="font-weight-bold">Coaching</p>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem i
+      </p>
+      <p color="font-weight-bold">Psychologische Beratung</p>
+      <p>
+        Haben Sie Erfahrungen in der Beratung von gewaltbetroffen Frauen oder
+        Frauenspezifischen Themen? Kennen Sie die Grundmuster von
+        Partnerschaftsgewalt und Gewaltdynamiken? Sind sie aufgrund ihres
+        beruflichen Hintergrundes in der Lage misshandelte Frauen zu
+        unterstützen sich aus ihrer Situation zu befreien?
+      </p>
+      <p color="font-weight-bold">Rechtsberatung</p>
+      <p>Sind sie Anwalt/Anwältin im Bereich Familienrecht?</p>
+    </v-container>
+    <v-divider />
+    <v-container class="text-center">
+      <h3 class="text-h3 primary--text">
+        so laufen Terminfindung und Beratung ab
+      </h3>
+      <div class="d-flex flex-column align-center">
+        <div v-for="(step, k) in steps" :key="k">
+          <v-avatar
+            size="60"
+            class="pa-12"
+            :style="
+              'border: 1px solid ' + $vuetify.theme.themes.light.secondary
+            "
+            ><v-icon color="primary">{{ step.icon }}</v-icon></v-avatar
+          >
+          <p class="caption">
+            {{ step.text }}
+          </p>
+        </div>
+      </div>
+    </v-container>
+    <v-divider />
+    <v-container>
+      <h2 class="text-h2 secondary--text">Fragen und Antworten</h2>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      steps: [
+        {
+          icon: 'mdi-eye',
+          text: 'Die Frauen suchen sich den/die für sie passende Berater*in/Coach*in aus.',
+        },
+        {
+          icon: 'mdi-speak',
+          text: 'An diese*n schreiben sie eine private Nachricht mit Terminanfrage, innerhalb unseres Portals.',
+        },
+        {
+          icon: 'mdi-calendar',
+          text: 'Sie können den Termin annehmen, ablehnen oder einen eigenen Vorschlag machen.',
+        },
+        {
+          icon: 'mdi-money',
+          text: 'Ist ein Termin gefunden, bezahlt die Frau die Beratung im Voraus.',
+        },
+        {
+          icon: 'mdi-letter',
+          text: 'Danach bekommen Sie beide eine E-Mail mit dem Termin und Einwahldaten zur Online-Beratung.',
+        },
+        {
+          icon: 'mdi-phone',
+          text: 'An Ihrem Termin wählen Sie sich beide in unsere Meeting-Software RED (was ist das?) ein und haben 1 Stunde Zeit, sich zu besprechen.',
+        },
+        {
+          icon: 'mdi-money',
+          text: 'Ist die Beratung beendet, erhalten Sie die Bezahlung.',
+        },
+      ],
+    }
+  },
+}
+</script>
