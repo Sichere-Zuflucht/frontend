e670077aa636e337abf31fb43b189229f4e5295b
ulrichbenedikt <ulrichbenedikt@gmail.com>
2021-09-20 18:02:19 +0200

    finish about us page + creating components

--- pages/info-berater.vue
+++ pages/info-berater.vue
@@ -175,17 +175,7 @@
           werden! <br /><br />Vielen Dank für ihr Engagement!
         </h3>
         <UtilsBtn text="Ja, ich möchte beraten" />
-        <v-img src="teaser-spenden.jpg" class="pa-8"
-          ><h2 class="white--text text-h2 text-center">
-            Sie möchten uns unterstützen!
-          </h2>
-          <p class="caption white--text text-center pt-4">
-            Sichere Zuflucht ist ein Team aus Freiwilligen. Dieses Hilfsangebot
-            entwickeln wir in unserer Freizeit und finanzieren es aus unseren
-            privaten Mitteln. Deshalb sind wir für jede Spende dankbar!
-          </p>
-          <UtilsBtn text="Jetzt spenden" classname="py-2" />
-        </v-img>
+        <UtilsDonation />
       </v-container>
     </v-sheet>
   </div>
