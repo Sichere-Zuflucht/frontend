ee7014f8b8269410666a413191c6a7bf80314e92
ulrichbenedikt <ulrichbenedikt@gmail.com>
2021-12-05 17:17:25 +0100

    is working with husky error

--- middleware/index.js
+++ middleware/index.js
@@ -1,47 +1,72 @@
-export default async function ({ store, redirect, route }) {
-  // this makes sure the cookies are loaded, only first time is blocking,
-  // because for continued calls the promise is already resolved
+export default async function ({ store, redirect, route, app }) {
   await store.restored
 
   // /register is responsible for verifying the email address
-  if (route.path === '/register' && !isSignInWithEmailLink(route))
-    redirect('/login')
-
-  // /update-profile asks the user for additional information
-  if (route.path === '/update-profile' && route.query.eMail === undefined) {
-    redirect('/login')
-  }
-
   if (
-    route.path === '/login' &&
-    store.getters['modules/user/isAuthenticated']
+    route.path === '/registration/confirm-register-link' &&
+    !isSignInWithEmailLink(route)
+  )
+    return redirect('/registration/signup')
+
+  // '/registration/membership-selection' asks the user for additional information
+  if (
+    route.path === '/registration/membership-selection' &&
+    route.query.eMail === undefined
   ) {
-    redirect('/')
+    return redirect('/registration/signup')
   }
 
-  redirectProfilePage(store, redirect, route)
+  if (requiresAuth(route) && !isAuthenticated(store)) {
+    console.log('requires auth')
+    return redirect('/registration/signin')
+  }
+
+  if (requiresWoman(route) && !isWoman(store)) {
+    console.log('requires woman')
+    return redirect('/')
+  }
+
+  if (requiresCoach(route) && !isCoach(store)) {
+    console.log('requires coach')
+    return redirect('/')
+  }
+
+  if (route.path === '/profile') {
+    return redirect(store.getters['modules/user/routing'])
+  }
 }
 
 function isSignInWithEmailLink(route) {
   if (process.client) {
+    // window.$nuxt.$fire is important here
     return window.$nuxt.$fire.auth.isSignInWithEmailLink(route.fullPath)
   }
   return false
 }
 
-function redirectProfilePage(store, redirect, route) {
-  if (route.path === '/profile') {
-    if (!store.getters['modules/user/isAuthenticated']) {
-      return redirect('/login')
-    }
+function requiresAuth(route) {
+  return ['frauen', 'settings', 'bezahlung', 'beratung'].includes(
+    route.path.split('/')[1]
+  )
+}
 
-    // coach did not enter info what coaching he wants to do
-    if (
-      store.getters['modules/user/membership'].id === 'Coach' &&
-      !store.getters['modules/user/user'].info
-    ) {
-      return redirect('/beratung/registrierung')
-    }
-    return redirect(store.getters['modules/user/routing'])
-  }
+function isAuthenticated(store) {
+  return !!store.getters['modules/user/isAuthenticated']
+}
+
+function requiresWoman(route) {
+  return route.path.startsWith('/frauen/')
+  // return ['/frauen'].includes(route.path)
+}
+
+function isWoman(store) {
+  return store.getters['modules/user/membership'] === 'Woman'
+}
+
+function requiresCoach(route) {
+  return route.path.startsWith('/beratung')
+}
+
+function isCoach(store) {
+  return store.getters['modules/user/membership'] === 'Coach'
 }
