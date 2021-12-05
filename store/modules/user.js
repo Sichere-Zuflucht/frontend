50aeade00b7c5e31f1d75997d0b55a4ffc80f865
ulrichbenedikt <ulrichbenedikt@gmail.com>
2021-12-05 17:13:24 +0100

    working with husky error

--- store/modules/user.js
+++ store/modules/user.js
@@ -1,123 +1,198 @@
 const state = () => ({
-  uid: null,
-  email: null,
-  emailVerified: null,
-  membership: null,
-  verifySetting: {
-    isVerifying: null,
-    verified: null,
-  },
+  private: null,
+  public: null,
+  claims: null,
 })
+
+function removeFirebaseObjects(obj) {
+  if (!!obj && typeof obj === 'object')
+    return Object.entries(obj)
+      .filter(([k, v]) => v?.firestore === undefined)
+      .reduce(
+        (r, [key, value]) => ({ ...r, [key]: removeFirebaseObjects(value) }),
+        {}
+      )
+  else return obj
+}
 
 const getters = {
   uid(state) {
-    if (state.uid) return state.uid
-    else return null
+    return state.public?.uid
   },
-
   user(state) {
     return state
   },
-
+  private(state) {
+    return state.private
+  },
+  public(state) {
+    return state.public
+  },
+  stripe(state) {
+    return state.private?.stripe
+  },
+  stripeDone(state) {
+    return (
+      state.private?.stripe.chargesEnabled &&
+      state.private?.stripe.payoutsEnabled
+    )
+  },
   isAuthenticated(state) {
-    return !!state.uid
+    return !!state.claims?.email_verified
   },
   membership(state) {
-    return state.membership
+    return state.claims?.membership
+  },
+  avatar(state) {
+    return state.public?.avatar
   },
   isVerifying(state) {
-    return state.verifySetting.isVerifying
+    return state.private?.verifySetting.isVerifying
   },
   verified(state) {
-    return state.verifySetting.verified
+    return state.private?.verifySetting.verified
   },
   routing(state) {
-    if (state.membership) return state.membership.routing
-    else return '/'
+    return state.claims?.routing || '/'
+  },
+  readyToShow(state) {
+    return (
+      state.private?.verifySetting.verified &&
+      state.private.stripe &&
+      state.public?.info
+    )
   },
 }
 
 const actions = {
-  onAuthStateChangedAction({ commit }, { authUser, claims }) {
+  async onAuthStateChangedAction({ commit, dispatch }, { authUser, claims }) {
     if (!authUser) {
-      // claims = null
       // Perform logout operations
       console.log('logg out', authUser)
       commit('setUserData', null)
+      commit('setClaims', null)
     } else {
       // Do something with the authUser and the claims object...
       console.log('logg in', authUser)
-      this.$fire.firestore
-        .collection('users')
-        .doc(authUser.uid)
-        .get()
-        .then((user) => {
-          const userData = user.data()
-          if (userData) {
-            commit('setUserData', userData)
-            userData.membership.get().then((doc) => {
-              commit('setMembership', doc.data())
-            })
-          }
-        })
+      await commit('setClaims', {
+        claims,
+      })
+      dispatch('fetchUserProfile', { user: authUser })
     }
   },
-  setInfo({ commit }, { info, uid }) {
-    commit('setInfo', info)
-    this.$fire.firestore.collection('users').doc(uid).update({ info })
+  async login({ dispatch }, form) {
+    // sign user in
+    const { user } = await this.$fire.auth.signInWithEmailAndPassword(
+      form.email,
+      form.password
+    )
+    // fetch user profile and set in state
+    dispatch('fetchUserProfile', { user, redirect: true })
   },
-  setVerify({ commit }, { uid, verifySetting }) {
-    commit('setVerify', verifySetting)
-    this.$fire.firestore.collection('users').doc(uid).update({ verifySetting })
-  },
-  async createFirebaseUser({ commit }, { uid, userData }) {
-    commit('setUserData', userData)
-    await userData.membership.get().then((doc) => {
-      commit('setMembership', doc.data())
+  fetchUserProfile({ commit }, { user, redirect = false }) {
+    console.log('fetchUser', user, redirect)
+    // fetch user profile
+
+    const userRef = this.$fire.firestore.collection('users/').doc(user.uid)
+
+    userRef.get().then((userDoc) => {
+      if (userDoc.exists) {
+        const _private = userRef
+          .collection('private')
+          .get()
+          .then((ref) => ref.docs[0].data())
+        const _public = userRef
+          .collection('public')
+          .get()
+          .then((ref) => ref.docs[0].data())
+
+        Promise.all([_private, _public]).then((p) => {
+          p[1].uid = user.uid
+          commit('setUserData', { _private: p[0], _public: p[1] })
+          p[1].membership.get().then((doc) => {
+            commit('setMembership', {
+              membership: doc.data(),
+              redirect,
+            })
+          })
+        })
+      }
     })
-    await this.$fire.firestore.collection('users/').doc(uid).set(userData)
+  },
+  setInfo({ commit }, info) {
+    this.$fire.functions
+      .httpsCallable('user-setData')({ data: { info }, priv: false })
+      .then(() => commit('setInfo', info))
+  },
+  setAvatar({ commit }, avatar) {
+    this.$fire.functions
+      .httpsCallable('user-setData')({ data: { avatar }, priv: false })
+      .then(() => commit('setAvatar', avatar))
+  },
+  requestVerify({ commit }, verifyData) {
+    return this.$fire.functions
+      .httpsCallable('email-sendVerifyAccMail')(verifyData)
+      .then((settings) => {
+        commit('setVerify', settings.data)
+      })
+  },
+  createFirebaseUser({ dispatch, commit }, userData) {
+    this.$fire.functions
+      .httpsCallable('user-create')(userData)
+      .then(({ data }) => {
+        commit('setClaims', {
+          claims: data,
+        })
+        dispatch('fetchUserProfile', { user: data, redirect: true })
+      })
   },
 }
 
 const mutations = {
-  setUserData(state, userData) {
-    console.log('[STORE MUTATIONS] - setUserData:', userData)
-    if (!userData) {
+  setAuthData(state, authData) {
+    console.log('[STORE MUTATIONS] - setAuthData:', authData)
+    const { uid, email, emailVerified } = authData
+    state.email = email
+    state.emailVerified = emailVerified
+    state.uid = uid
+  },
+  setMembership(state, data) {
+    console.log('[STORE MUTATIONS] - setMembership:', data)
+    const { membership, redirect } = data
+    state.public.membership = membership
+    if (redirect) this.$router.push({ path: '/profile' })
+  },
+  setUserData(state, data) {
+    console.log('[STORE MUTATIONS] - setUserData:', data)
+    if (!data?._private || !data?._public) {
       for (const key of Object.keys(state)) {
         state[key] = null
       }
     } else {
-      for (const entry of Object.entries(userData)) {
-        // filter out firestore objects (like membership)
-        if (entry[1].firestore === undefined) {
-          state[entry[0]] = entry[1]
-        }
-      }
+      const { _private, _public } = data
+      state.private = removeFirebaseObjects(_private)
+      state.public = removeFirebaseObjects(_public)
     }
   },
   setInfo(state, info) {
     console.log('[STORE MUTATIONS] - setInfo:', info)
-    state.info = info
+    state.public.info = info
+  },
+  setAvatar(state, avatar) {
+    console.log('[STORE MUTATIONS] - setAvatar:', avatar)
+    state.public.avatar = avatar
   },
   setVerify(state, verifySetting) {
-    console.log('[STORE MUTATIONS] - setVerify:', state, verifySetting)
-    state.verifySetting = verifySetting
+    console.log('[STORE MUTATIONS] - setVerify:', verifySetting)
+    state.private.verifySetting = verifySetting
   },
-  setMembership(state, membership) {
-    console.log('[STORE MUTATIONS] - setMembership:', membership)
-    state.membership = membership
-  },
-  ON_AUTH_STATE_CHANGED_MUTATION(state, { authUser, claims }) {
-    console.log('ON_AUTH_STATE_CHANGED_MUTATION', authUser, state)
-    if (authUser) {
-      const { uid, email, emailVerified } = authUser
-      state.email = email
-      state.emailVerified = emailVerified
-      state.uid = uid
+  setClaims(state, data) {
+    console.log('[STORE MUTATIONS] - setClaims:', data)
+    if (!data) {
+      state.claims = null
     } else {
-      state.email = null
-      state.emailVerified = null
-      state.uid = null
+      const { claims } = data
+      state.claims = claims
     }
   },
 }
